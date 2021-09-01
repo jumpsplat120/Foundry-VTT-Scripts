ba0d6316a1188c40f58c43be2b244d089fa5939e
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 16:18:12 -0700

    i -> ff

--- macros/favoured_foe.js
+++ macros/favoured_foe.js
@@ -5,6 +5,6 @@ const ff = utils.getItemByName("Favored Foe");
 const damage = ff.data.data.damage.parts[0][0];
 
 ff.roll({configureDialog: false, createMessage: false}).then(chat_message => { if (chat_message) {
-    utils.chatMessage({ img: i.img, content: i.name }, { small: `On a successful damage dealing attack, Taylor may mark an enemy for one minute (or until she loses concentration), and deal an extra ${damage} damage to it.` })
+    utils.chatMessage({ img: ff.img, content: ff.name }, { small: `On a successful damage dealing attack, Taylor may mark an enemy for one minute (or until she loses concentration), and deal an extra ${damage} damage to it.` })
     track.damage.favored_foe = [false, null, false];
 }});

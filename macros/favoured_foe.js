1fc3208a44123b6b1dfe7e10a9626dafe4c20cf5
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 16:09:41 -0700

    Add favored_foe

---
+++ macros/favoured_foe.js
@@ -0,0 +1,10 @@
+const saved = game.saved_macro;
+const utils = saved.utils;
+const track = saved.tracking;
+const ff = utils.getItemByName("Favored Foe");
+const damage = ff.data.data.damage.parts[0][0];
+
+ff.roll({configureDialog: false, createMessage: false}).then(chat_message => { if (chat_message) {
+    u.chatMessage({ img: i.img, content: i.name }, { small: `On a successful damage dealing attack, Taylor may mark an enemy for one minute (or until she loses concentration), and deal an extra ${damage} damage to it.` })
+    track.damage.favored_foe = [false, null, false];
+}});

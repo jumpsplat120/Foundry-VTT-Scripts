12e6b69cb3e2004559d91c81289dedd634233213
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 15:44:29 -0700

    Add lucky macro

---
+++ macros/lucky.js
@@ -0,0 +1,12 @@
+const saved = game.saved_macro;
+const utils = saved.utils;
+const track = saved.tracking;
+const lucky = utils.getItemByName("lucky");
+const labels = lucky.labels;
+
+lucky.roll({configureDialog: false, createMessage: false}).then(chat_message => {
+	if (chat_message) { utils.chatMessage({ img: lucky.img, content: lucky.name }, { small: `After spending a luck point, Taylor may roll an additional d20 and choose which die to use.` }) }
+});
+
+track.advantage[track.advantage.length] = [lucky.name, 1, "hand-sparkles", function() { track.lucky = false; }];
+track.lucky = true;

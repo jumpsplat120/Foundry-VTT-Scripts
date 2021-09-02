d913dab520cb741adf3bbf97174fc3180ae75388
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 17:00:32 -0700

    Adding naming via targetting (double right click)

--- macros/favoured_foe.js
+++ macros/favoured_foe.js
@@ -2,9 +2,8 @@ const saved = game.saved_macro;
 const utils = saved.utils;
 const track = saved.tracking;
 const ff = utils.getItemByName("Favored Foe");
-const damage = ff.data.data.damage.parts[0][0];
 
 ff.roll({configureDialog: false, createMessage: false}).then(chat_message => { if (chat_message) {
-    utils.chatMessage({ img: ff.img, content: ff.name }, { small: `On a successful damage dealing attack, Taylor may mark an enemy for one minute (or until she loses concentration), and deal an extra ${damage} damage to it.` })
+    utils.chatMessage({ img: ff.img, content: ff.name }, { small: `Taylor has marked ${game.user.targets.size > 0 ? [...game.user.targets][0].name : "an enemy"}! On a successful damage dealing attack, for one minute (or until she loses concentration), she may deal an extra <b>${ff.data.data.damage.parts[0][0]}</b> damage to it.` })
     track.damage.favored_foe = [false, null, false];
 }});

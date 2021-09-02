bec30b35b85184f7db09265a23f2c501c8acb0ed
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-01 17:02:56 -0700

    Target name will show up in title

--- macros/favoured_foe.js
+++ macros/favoured_foe.js
@@ -2,8 +2,9 @@ const saved = game.saved_macro;
 const utils = saved.utils;
 const track = saved.tracking;
 const ff = utils.getItemByName("Favored Foe");
+const name = game.user.targets.size > 0 ? [...game.user.targets][0].name : false;
 
 ff.roll({configureDialog: false, createMessage: false}).then(chat_message => { if (chat_message) {
-    utils.chatMessage({ img: ff.img, content: ff.name }, { small: `Taylor has marked ${game.user.targets.size > 0 ? [...game.user.targets][0].name : "an enemy"}! On a successful damage dealing attack, for one minute (or until she loses concentration), she may deal an extra <b>${ff.data.data.damage.parts[0][0]}</b> damage to it.` })
+    utils.chatMessage({ img: ff.img, content: ff.name + `${name ? " (" + name + ")" : ""}` }, { small: `Taylor has marked <b>${name ? name : "an enemy"}</b>! On a successful damage dealing attack, for one minute (or until she loses concentration), she may deal an extra <b>${ff.data.data.damage.parts[0][0]}</b> damage to it.` })
     track.damage.favored_foe = [false, null, false];
 }});

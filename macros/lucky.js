04b3b0a8dbf4714541ca187bac6ba765ba39c0b7
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-06 16:46:04 -0700

    Add a check so you can't activate lucky if you already have a use

--- macros/lucky.js
+++ macros/lucky.js
@@ -4,9 +4,19 @@ const track = saved.tracking;
 const lucky = utils.getItemByName("lucky");
 const labels = lucky.labels;
 
-lucky.roll({configureDialog: false, createMessage: false}).then(chat_message => {
-	if (chat_message) { utils.chatMessage({ img: lucky.img, content: lucky.name }, { small: `After spending a luck point, Taylor may roll an additional d20 and choose which die to use.` }) }
-});
-
-track.advantage[track.advantage.length] = [lucky.name, 1, "hand-sparkles", function() { track.lucky = false; }];
-track.lucky = true;
+if (track.lucky) {
+	Dialog.prompt({
+		title: "Lucky Prompt",
+		content: "You already have a use of Lucky!",
+		label: "OK",
+		callback: function() {},
+		rejectClose: false
+	});
+} else {
+	lucky.roll({configureDialog: false, createMessage: false}).then(chat_message => {
+		if (chat_message) { utils.chatMessage({ img: lucky.img, content: lucky.name }, { small: `After spending a luck point, Taylor may roll an additional d20 and choose which die to use.` }) }
+	});
+	
+	track.advantage[track.advantage.length] = [lucky.name, 1, "hand-sparkles", function() { track.lucky = false; }];
+	track.lucky = true;
+}

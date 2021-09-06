3aa6df7d8d1989bb6f0d0c75099d4cf3a106f5ec
jumpsplat120 <jumpsplat120@yahoo.com>
2021-09-06 15:20:24 -0700

    Update buttons, fix push_foe

--- macros/gathered_swarm.js
+++ macros/gathered_swarm.js
@@ -9,47 +9,35 @@ const intro = "A swarm of skittering spiders and flying insects assist the attac
 new Dialog({
     title: "Select Swarm Option",
     buttons: {
-        damage: {
-            icon: '<i class="fas fa-fist-raised"></i>',
-            label: "Deal Damage",
-            callback: event => {
-                utils.chatMessage({ img: gs.img, content: gs.name + " (Damage)" }, { small: `${intro} On a <b>successful</b> hit, the attack deals an additional <b>1d6</b> piercing damage.` })
-                track.damage.gathered_swarm = [false, null, false];
+        damage: utils.createButton("Deal Damage", "fist-raised", event => {
+            utils.chatMessage({ img: gs.img, content: gs.name + " (Damage)" }, { small: `${intro} On a <b>successful</b> hit, the attack deals an additional <b>1d6</b> piercing damage.` })
+            track.damage.gathered_swarm = [false, null, false];
+        }),
+        push_foe: utils.createButton("Move Enemy", "angle-double-right", event => {
+            if (target) {
+                target.actor.rollAbilitySave("str", { fastForward: true, chatMessage: false }).then(roll => {
+                    utils.chatMessage({ img: gs.img, content: gs.name + ` (Move ${name})` }, { small: `${intro} On a <b>successful</b> hit, ${name} must roll a Strength Saving throw against Taylor's Spell Save DC (<b>${game.user.character.data.data.attributes.spelldc}</b>) or be moved up to 15 feet horizontally.` }, [{
+                        title: `${name}'s Strength Saving Throw`,
+                        onclick: function(button, roll) {
+                            const content = button.parentElement.parentElement.parentElement;
+                            button.classList.add("nohover");
+                            button.innerHTML = `<h3 style="font-weight: bold;">${roll.total}</h3>`;
+                        },
+                        data: [ roll ]
+                    }])
+                });
+            } else {
+                Dialog.prompt({
+                    title: "Missing Target",
+                    content: "Move Enemy requires an enemy to be targeted!",
+                    label: "OK",
+                    callback: function() {},
+                    rejectClose: false
+                });
             }
-        },
-        push_foe: {
-            icon: '<i class="fas fa-angle-double-right"></i>',
-            label: "Move Enemy",
-            callback: event => {
-                if (target) {
-                    target.actor.rollAbilitySave("str", { fastForward: true, chatMessage: false }).then(roll => {
-                        utils.chatMessage({ img: gs.img, content: gs.name + ` (Move ${name})` }, { small: `${intro} On a <b>successful</b> hit, ${name} must roll a Strength Saving throw against Taylor's Spell Save DC (<b>${game.user.character.data.data.attributes.spelldc}</b>) or be moved up to 15 feet horizontally.` }, [{
-                            title: `${name}'s Strength Saving Throw`,
-                            onclick: function(button, roll) {
-                                const content = button.parentElement.parentElement.parentElement;
-                                button.classList.add("nohover");
-                                button.innerHTML = `<h3 style="font-weight: bold;">${roll.total}</h3>`;
-                            },
-                            data: [ roll ]
-                        }])
-                    });
-                } else {
-                    Dialog.prompt({
-                        title: "Missing Target",
-                        content: "Move Enemy requires an enemy to be targeted!",
-                        label: "OK",
-                        callback: function() {},
-                        rejectClose: false
-                    });
-                }
-            }
-        },
-        push_self: {
-            icon: '<i class="fas fa-walking"></i>',
-            label: "Move Yourself",
-            callback: event => {
-                utils.chatMessage({ img: gs.img, content: gs.name + ` (Move ${name || "Enemy"})` }, { small: `${intro} On a <b>successful</b> hit, the swarm moves Taylor 5 feet horizontally.` })
-            }
-        }
+        }),
+        push_self: utils.createButton("Move Yourself", "walking", event => {
+            utils.chatMessage({ img: gs.img, content: gs.name + ` (Move ${name || "Enemy"})` }, { small: `${intro} On a <b>successful</b> hit, the swarm moves Taylor 5 feet horizontally.` })
+        })
     }
 }).render(true);

85c3a9329d57e54724edcca35c9d304a14957f42
jumpsplat120 <jumpsplat120@yahoo.com>
2022-06-30 10:15:03 -0700

    Update arrow_recovery.js
    Updated to modern utils

--- macros/arrow_recovery.js
+++ macros/arrow_recovery.js
@@ -1,19 +1,12 @@
-const s = game.saved_macro;
-const u = s.utils;
+const arrows    = utils.getItemByName("arrows");
+const recovered = Math.round(utils.tracking.arrows / 2);
 
-const arrows = u.getItemByName("arrows");
-let fired_arrows = s.tracking.arrows;
-const recovered = Math.round(fired_arrows / 2);
-fired_arrows = 0;
+utils.tracking.arrows = 0;
 
-u.updateItemQuantity("arrows", recovered, "+");
+utils.increaseItemQuantity(arrows, recovered);
 
-u.chatMessage(null, { small: "...At the end of the battle, you can recover half your expended ammunition by taking a minute to search the battlefield."}, [{
-	title: "Recover",
-	onclick: function(button, recovered) {
-		const content = button.parentElement.parentElement.parentElement;
-		button.classList.add("nohover");
-		button.innerHTML = `<h3 style="font-weight: bold;">${recovered}</h3>`;
-	},
-	data: [ recovered ]
-}], null, "Ammo Recovery");
+new utils.Message()
+	.setFlavor("Arrow Recovery")
+	.setCardContent("At the end of the battle, you can recover half your expended ammunition by taking a minute to search the battlefield.")
+	.addDieTotal(recovered)
+	.send();

d2657a35a66bd0dec1a7bb90e7b40d57bf14bbc7
jumpsplat120 <jumpsplat120@yahoo.com>
2022-07-08 14:50:54 -0700

    Flattened promise chain
    data was pulled up a layer, so that way we could flatten the promise chain. Also, we check at every step if the returned value is null, and just keep passing it down, if so.

--- macros/dagger_attack.js
+++ macros/dagger_attack.js
@@ -10,39 +10,28 @@ const dex       = abilities.dex.mod;
 const str       = abilities.str.mod;
 const finess    = dex >= str;
 
+let pool_data;
+
 utils.Advantage.check()
 	.then(sources => {
 		if (sources === null) { return null; }
 		
-		const data = utils.Advantage.pool(sources);
+		pool_data = utils.Advantage.pool(sources);
 
-		//play sound, then roll
-		return utils.playSound(utils.sounds?.dagger_swing?.random).then(_ => new utils.Roll(dagger)
-			.add(data.formula, data.label, "Base")
+		return utils.playSound(utils.sounds?.dagger_swing?.random);
+	})
+	.then(_ => {
+		if (_ === null) { return null; }
+
+		return new utils.Roll(dagger)
+			.add(pool_data.formula, pool_data.label, "Base")
 			.maybeAdd(finess, dex, "Dexterity (Finess)", "Ability Modifier")
 			.maybeAdd(!finess, str, "Strength", "Ability Modifier")
 			.maybeAdd(item_data.proficient, item_data.prof.term, "Simple Weapons", "Proficency")
-			.roll())
+			.roll()
 	})
-	.then(message => message == null ? null : message.setFlavor("Attack Roll").show());
+	.then(message => {
+		if (message === null) { return null; }
 
-
-//const lucky_dialog = u.luckyPrompt(event => {
-//    rollAttackAndPlaySound(null, null, "Dagger w/ Lucky (Reroll) - Attack Roll");
-//	t.lucky = false;
-//	t.advantage = t.advantage.filter(arr => arr[0] != "Lucky");
-//}, event => {
-//	if (t.advantage.length > 0) {
-//		advantageDialog();
-//	} else {
-//        rollAttackAndPlaySound(event.shiftKey, event.ctrlKey);
-//	}
-//})
-//
-//if (t.lucky && t.damage.dagger) {
-//    lucky_dialog.render(true);
-//} else if (t.advantage.length > 0) {
-//    advantageDialog();
-//} else {
-//    rollAttackAndPlaySound(event.shiftKey, event.ctrlKey);
-//}
+		return message.setFlavor("Attack Roll").show();
+	});

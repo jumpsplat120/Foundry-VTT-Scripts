5e542b0fab25e61c3efe52006a623821b2c5d7ce
jumpsplat120 <jumpsplat120@yahoo.com>
2022-07-11 20:40:58 -0700

    Using new promise stuff for conciseness
    Sharpshooter check happens before advantage check, but uses `maybe` from Dialog.show to not run if the item isn't proficent

--- macros/longbow_attack.js
+++ macros/longbow_attack.js
@@ -8,57 +8,44 @@ const arrows    = utils.getItemByName("arrows");
 const item_data = longbow.data.data;
 const abilities = game.user.character.data.data.abilities;
 const str       = abilities.str.mod;
-let pool_data;
-let sharpshooter;
+
+let sharpshooter = false;
 
 if (utils.getItemByName("longbow").data.data.quantity <= 0) {
 	new utils.Dialog.ok("Ammo Notification", "You don't have any arrows for your bow!");
 	return;
 }
 
-utils.Advantage.check()
-	.then(sources => {
-		if (sources === null) { return null; }
-		
-		pool_data = utils.Advantage.pool(sources);
-
-		return utils.playSound(utils.sounds?.bow_draw?.random);
-	})
-	.then(_ => {
-		if (_ === null) { return null; }
-
-		if (!(item_data.proficient && !!utils.getItemByName("Sharpshooter"))) { return false; }
-
-		//if proficent, and have the sharpshooter feat...
-		return new utils.Dialog("Sharpshooter", "You have the sharpshooter feat! If you'd like, you can take a -5 to your attack, and if the attack hits, you gain a +10 in damage.")
-			.addButton(new utils.Button()
-				.setIcon("bullseye")
-				.setText("Heck Ya")
-				.setCallback(_ => { sharpshooter = true; }))
-			.addButton(new utils.Button()
-				.setIcon("times")
-				.setText("Nah")
-				.setCallback(_ => { sharpshooter = false; }))
-			.onClose(_ => null)
-			.show()
-	})
-	.then(_ => {
-		if (_ === null) { return null; }
+new utils.Dialog("Sharpshooter", 
+"You have the sharpshooter feat! If you'd like, you can take a -5 to your attack, and if the attack hits, you gain a +10 in damage.")
+	.addButton(new utils.Button()
+		.setIcon("bullseye")
+		.setText("Heck Ya")
+		.setCallback(utils.Button.yes))
+	.addButton(new utils.Button()
+		.setIcon("times")
+		.setText("Nah")
+		.setCallback(utils.Button.no))
+	.onClose((_, r) => r("Sharpshooter dialog closed."))
+	.show(!!utils.getItemByName("Sharpshooter") && item_data.proficient)
+	.then(choice => { sharpshooter = choice; }, utils.reject)
+	.then(utils.Advantage.check, utils.reject)
+	.then(utils.Advantage.pool, utils.reject)
+	.then(data => {
+		utils.playSound(utils.sounds?.bow_draw?.random);
 
 		return new utils.Roll(longbow)
-			.add(pool_data.formula, pool_data.label, "Base")
+			.add(data.formula, data.label, "Base")
 			.add(str, "Strength", "Ability Modifier")
 			.maybeAdd(item_data.proficient, item_data.prof.term, "Martial Weapons", "Proficency")
 			.maybeAdd(utils.getItemByName("Fighting Style: Archery"), 2, "Fighting Style - Archery", "Feat")
 			.maybeSubtract(sharpshooter, 5, "Sharpshooter", "Feat")
 			.roll()
-	})
+	}, utils.reject)
 	.then(message => {
-		if (message === null) { return null; }
-
 		utils.decreaseItemQuantity(arrows, 1);
 		
 		utils.tracking.arrows++;
 
 		return message.setFlavor("Attack Roll").show();
-	});
+	}, utils.reject);
